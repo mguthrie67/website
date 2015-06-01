98b2e03b383e96bbc9f69dfea16b41ba4c4b0195
Mark Guthrie <Mark.Guthrie@17ways.com.au>
2015-06-01 17:00:45 +1000

    Stuff

--- js/custom.js
+++ js/custom.js
@@ -210,3 +210,12 @@ $(document).ready(function () {
 
 
 
+function toggle(elementId) {
+    var ele = document.getElementById(elementId);
+    if(ele.style.display == "block") {
+        ele.style.display = "none";
+    }
+    else {
+        ele.style.display = "block";
+    }
+}
