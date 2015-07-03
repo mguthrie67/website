f4867402407da8d315e086d646eaed4d7f7378ba
Mark Guthrie <mark@drunkguthrie.com>
2015-07-04 07:01:26 +1000

    .

--- tools/campaign_step1.php
+++ tools/campaign_step1.php
@@ -1,7 +1,7 @@
 <?php include('_header.html');?>
 
 
-<h1>Create New Campaign</h1>
+<h1>Create New Campaign - Step 1</h1>
 
 
 
@@ -21,24 +21,30 @@ $next=sprintf("%'.03d", $top);
 echo '<input type="hidden" name="campaign" value="' . $next . '">';
 ?>
 
+
 <table>
     <tr>
         <td>Title
         <td><input class="span7" type="text" name="title" value=""  required/>
     </tr>
     <tr>
-        <td>Where
-        <td><input class="span7" type="text" name="where" value=""  required/>
+        <td>Description
+        <td><textarea input class="span7" name="description" value=""  rows="6" cols="30" required/></textarea>
     </tr>
+    <tr>
     <tr>
         <td>When
         <td><input class="span7" type="text" name="when" value="" placeholder="Change me to a date and time  picker" required/>
     </tr>
-    <tr>
-        <td>Description
-        <td><textarea input class="span7" name="description" value=""  rows="6" cols="30" required/></textarea>
+        <td>Where
+        <td><input class="span7" type="text" name="where" value=""  required/>
     </tr>
 </table>
+
+<div id="map"><iframe width="100%" height="310" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                      src="https://www.google.com.au/maps/place/The+Royal+George,+320+George+St,+Sydney+NSW+2000"></iframe><br />
+</div>
+
 
             <div class="clear"></div>
             <input type="submit" class="contact_btn" value="Submit" />
