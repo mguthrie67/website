8f69525a754910f3e9a46befd8afa91bf916a2c8
mguthrie67 <mark@drunkguthrie.com>
2015-07-21 23:03:41 +1000

    .

---
+++ services/cloud.php
@@ -0,0 +1,87 @@
+<?php include("_header.html"); ?>
+
+<h1>Cloud for the Real World</h1>
+
+<p>Cloud technology offers real opportunity for companies that can implement it correctly. But if you pick the wrong
+use cases or fail to follow through and retire your legacy systems then cloud can be a very expensive folly.
+</p>
+<p>
+Let <a href="../index.html">17 Ways</a> help you to get it right.
+</p>
+<img src="../images/services/umbrella.jpg" alt="Cloud IT Umbrella" width="400px">
+<blockquote>We'll hold the umbrella for you to get through the initial storms and help you to become successful.
+</blockquote>
+<p>
+It is probably best to describe us as "Cloud Cynics". That doesn't mean that we are against cloud technology in any way,
+in fact we are long time supporters and use it for all of our own infrastructure, it's more a reflection of our experience and the fact
+that we have seen the dangers in getting it wrong by attempting to put the wrong things into the cloud or failing to understand
+the full scope of what needs to be done to be successful.
+</P>
+<blockquote>Cloud is a great addition to your options as an IT professional. We can help you to use it for the right things and
+follow through to get the best outcome.
+</blockquote>
+<p>
+For a fuller description of our views on cloud please read our CEO article <a href="../Library/CEO/Cloud%20vs%20Converged.html">Cloud vs Converged.</a>
+<h2>Our Experience</h2>
+A few examples of our successes with cloud so far:
+<dl>
+    <dt>SaaS (Trading)</dt>
+        <dd>Implemented 3 different SaaS solutions for trading systems in the Middle East, Australia and New Zealand between 2005 and 2011.</dd>
+    <dt>SaaS (Salesforce)</dt>
+        <dd>Integrated Salesforce into an inhouse FX trading application used at multiple companies.</dd>
+    <dt>IaaS</dt>
+        <dd>Built the internal VDI and VMWare farm solutions for a major Investment Bank.</dd>
+    <dt>IaaS</dt>
+        <dd>Currently advising an Australian household name on a cloud migration program.</dd>
+    <dt>PaaS</dt>
+        <dd>Built the Oracle, Sybase and SQLServer solutions for a major Investment Bank.</dd>
+
+ </dl>
+
+<h2>What We Have Learnt</h2>
+<p>Here are 3 of the main things to consider:</p>
+<ol>
+<li>As with all technologies, consider the options and make a decision based on the head and not the heart. </li>
+<li>Don't worry about what failure will look like, worry about what success will look like. How will you need to change WHEN cloud becomes mainstream for you.
+Dipping your toe in is a precursor to a full dive. How will your organisation look once you are immersed.</li>
+<li>Do a full cost benefit case and plan to follow through with any cost savings that involve retiring old systems or restructuring your teams.</li>
+</ol>
+
+<h2>How We Can Help</h2>
+<dl>
+    <dt>End-to-End Analysis of Cost and Benefit</dt>
+        <dd>We can give you the whole picture including ROI and TCO analysis that includes the knock-on effects such as impact on other
+            teams and early termination of existing contracts.</dd>
+    <dt>Vendor Management</dt>
+        <dd>With cloud adoption comes the need to depend upon a vendor for your critical systems. We have experience with initial negotiations,
+        getting the best deal (and price isn't the key factor), setting up the relationship for ongoing success and planning for termination
+        and change of vendors. We can help you with the whole lifecycle and if you involve us early we can set it up for success and smooth running.
+        </dd>
+    <dt>Selling the Change</dt>
+        <dd>Any major change needs a lot of internal, and potentially external, selling. We have experience of selling
+        change at all levels of large scale organisations and can help you to frame the right message so it is properly received.
+        </dd>
+    <dt>Choosing A Partner</dt>
+        <dd>No we aren't a dating agency but it feels a bit like that when you are choosing a partner for your cloud solutions.
+        We can ask the right questions to find someone who will be a good partner for you. We know the main players well and can find a good fit for you.
+        </dd>
+    <dt>Following Through</dt>
+        <dd>Most cloud pitches include $xM of cost saving based upon eliminating processes, functions or headcount. We have done this for
+        other organisations and can help you to realise your predicted benefits with appropriate implementations.
+        </dd>
+    <dt>Alternative</dt>
+        <dd>You wouldn't look at a house and buy it without considering others. Committing to the cloud is a major undertaking and you should
+        look at the alternatives, even if it is only to be sure that you are doing the right thing. We are experts in converged and hyper-converged
+        systems. Even if you only use us for your free half day of consulting, we can help you to review your cloud solution to make sure it really\
+        is right for the problems that you face.
+        </dd>
+    <dt>Cloud Rescue</dt>
+        <dd>Even if you are having problems with your cloud implementation, it isn't too late to involve us. We are specialists at rescue. One of us
+         even worked at the fire brigade so we are confident that if you call us, even if it is late in the process, we can help you.
+         </dd>
+</dl>
+
+
+
+<?php include("_footer.html"); ?>
+
