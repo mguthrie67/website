9ffb7edd92508fd91782da13f2445a4be9deb557
mguthrie67 <mark@drunkguthrie.com>
2015-07-21 18:02:38 +1000

    Delete db.php

--- tools/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("DB_HOST", "new-sh8-eqx3-syd.ha-node.net");
-define("DB_NAME", "wayscoma_campaign_dev");
-define("DB_CAMPAIGN", "wayscoma_campaign_dev");
-define("DB_USER", "wayscoma_login");
-define("DB_PASS", "Jennie6g");
-define("EMAIL_GROUP","mark.guthrie@17ways.com.au");
-define("INSIGHTLY_KEY","1b59c7a6-98cc-4788-b4ae-d063453e04ab");
-?>
