441d40b5f161c20b74c5cb4b8a87aaf06fb952cc
Mark Guthrie <Mark.Guthrie@17ways.com.au>
2015-09-04 11:10:35 +1000

    .

--- tools/views/logged_in.php
+++ tools/views/logged_in.php
@@ -1,5 +1,3 @@
-<!-- if you need user information, just put them into the $_SESSION variable and output them here -->
 <a href="campaign_step1.php">Create New Campaign</a>
 
-<!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
-<a href="index.php?logout">Logout</a>
+
